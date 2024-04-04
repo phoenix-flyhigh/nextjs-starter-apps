cf90a58fcf9b3853a65750f2d29ebedbf8b0470c
phoenix-flyhigh <j.akshaya07@gmail.com>
2024-04-04 09:27:38 +0530

    Initial starter setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-blue-300 font-bold text-lg">Hello world</h1>
+    </main>
+  )
+}
